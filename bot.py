29ec9b9a1e08fac6aa4542302c97fbf576443931
karri <husnoraxamitova@gmail.com>
2025-03-27 10:28:20 +0500

    session qoshildi

--- bot.py
+++ bot.py
@@ -10,15 +10,16 @@ from aiogram.enums import ParseMode
 from bot_menu import set_bot_menu
 from handlers.commands import router as commands_router
 from handlers.handlers import router as handlers_router
-
+from aiogram.client.session.aiohttp import AiohttpSession
 load_dotenv()
-TOKEN = getenv("BOT_TOKEN")
+session = AiohttpSession(proxy="http://proxy.server:3128")
 
+TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
-
-async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+async def main(BOT_TOKEN=None) -> None:
+    # bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(token=BOT_TOKEN, session=session)
     dp.include_router(commands_router)
     dp.include_router(handlers_router)
     await set_bot_menu(bot)
